83f2cbebc423978800ed6f33a48e13fa1922cf14
msleon1997 <marlonslb@outlook.com>
2025-04-30 18:05:11 -0500

    creacion proyecto de automatizacion

---
+++ src/test/java/com/cat/automation_test/AutomationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cat.automation_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
